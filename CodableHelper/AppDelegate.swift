92150ed98988fdbfd4b619bc57d52798a801c689
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2023-10-08 09:49:59 +0800

    Initial Commit

---
+++ CodableHelper/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CodableHelper
+//
+//  Created by Jiaxin Pu on 2023/10/8.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CodableHelper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CodableHelper
+//
+//  Created by Jiaxin Pu on 2023/10/8.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
